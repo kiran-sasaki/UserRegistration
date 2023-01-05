caf2091f336024a22ed9fdf030e9c6cdc3f75e0f
kiran-sasaki <kiransekar1111@gmail.com>
2023-01-05 11:28:29 +0530

    [Kiran] Add : Welcome message

---
+++ UserRegistrationAssignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistrationAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To UserRegistration Assignment");
+        }
+    }
+}
