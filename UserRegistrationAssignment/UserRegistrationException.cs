0cf8e362d02973f0ea6cced67485f64d1c9cfbf2
kiran-sasaki <kiransekar1111@gmail.com>
2023-01-05 15:52:55 +0530

    [Kiran] Add : Custom Execption

---
+++ UserRegistrationAssignment/UserRegistrationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationAssignment
+{
+    public class UserRegistrationException : Exception
+    {
+        public enum ExceptionType
+        {
+            EMPTY_MESSAGE,INVALID_FIRSTNAME,INVALID_LASTNAME,INVALID_PHONENUMBER,INVALID_EMAIL_ADDRESS,INVALID_PASSWORD
+        }
+        public ExceptionType Type;
+        public UserRegistrationException(ExceptionType type,string message):base(message)
+        {
+            this.Type=type;
+        }
+    }
+}
