5c87cc89723eb42f90c5d235738d6fa437c14f7b
kiran-sasaki <kiransekar1111@gmail.com>
2023-01-05 11:46:28 +0530

    [Kiran] Add : Regex For EmailAddress

--- UserRegistrationAssignment/UserDetials.cs
+++ UserRegistrationAssignment/UserDetials.cs
@@ -13,6 +13,8 @@ namespace UserRegistrationAssignment
         public static string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
         //Regular Expression For LastName Validation
         public static string REGEX_LASTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+        //Regular Expression For Email Validation
+        public static string REGEX_EMAIL_ADDRESS = "^[A-Za-z0-9]([-_+.]{0,1}[A-Za-z0-9])*[@]${1}[A-Za-z0-9]{1,}[.]{1}[A-Za-z]{2,3}([.]{1}[a-zA-Z]{2,3}){0,1}";
         public bool ValidateFirstname(string firstname)
         {
             return Regex.IsMatch(firstname, REGEX_FIRSTNAME);
@@ -21,6 +23,10 @@ namespace UserRegistrationAssignment
         public bool ValidateLastname(string lastname)
         {
             return Regex.IsMatch(lastname, REGEX_LASTNAME);
+        }
+        public bool validateEmailAddress(string emailAddress)
+        {
+            return Regex.IsMatch(emailAddress, REGEX_EMAIL_ADDRESS);
         }
     }
 }
